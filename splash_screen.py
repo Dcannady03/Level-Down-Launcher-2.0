843ad17e32a13b6738d22c9e2ef4a80b829fbde3
Dcannady03 <132225017+Dcannady03@users.noreply.github.com>
2024-12-24 09:51:16 -0800

    s

--- splash_screen.py
+++ splash_screen.py
@@ -8,6 +8,8 @@ import hashlib
 import shutil
 import atexit  # Import atexit for cleanup
 import subprocess
+
+os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = os.path.join(os.getcwd(), "PyQt5", "Qt", "plugins", "platforms")
 # Register a cleanup function to avoid errors
 def cleanup_temp():
     temp_dir = getattr(sys, '_MEIPASS', None)
